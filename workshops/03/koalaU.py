3e0fdc7fb8c2b70137f0a288cd046dc9ef1b749f
namntran <namtran78@gmail.com>
2020-03-09 21:04:19 +1000

    add Koala university scoring program

---
+++ workshops/03/koalaU.py
@@ -0,0 +1,15 @@
+score = float(input("How many marks? "))
+# Calculate grade of each student 
+
+if score > 100:
+    print("invalid score")
+elif score >= 90: 
+        print("gum leaf cluster")
+elif score >= 60: 
+        print("leafy twig")
+elif score >= 45: 
+         print("gum leaf")
+elif score <= 45: 
+        print("dead twig")
+else: 
+        print("invalid")
