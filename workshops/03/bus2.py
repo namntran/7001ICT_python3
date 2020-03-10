498aebc26d0f95a6bbf81d648fe8dd5bd2c6ade4
namntran <namtran78@gmail.com>
2020-03-10 20:09:31 +1000

    program to calculate the number of buses for rugby tournament

---
+++ workshops/03/bus2.py
@@ -0,0 +1,11 @@
+# calculate number of buses for rugby tournament
+teams = float(input("How many teams? "))
+rugbyTeam = float(15)
+bigBus = float(38)
+
+buscapacity = teams*rugbyTeam/bigBus
+#if bus capacity not equal to whole number
+if buscapacity != int:
+    print(int(buscapacity+1))
+else:
+    print(int(buscapacity))
