016a888e359546f345bda77b145ba8fdf1141e9f
namntran <namtran78@gmail.com>
2020-03-14 14:58:10 +1000

    refactor

--- lectures/printGridTriangle.py
+++ lectures/printGridTriangle.py
@@ -1,9 +1,5 @@
 # ask for the number of rows and print the triangle
 
 rows = int(input("How many rows: "))
-hashes = 1
 for i in range(rows):
-    spaces = rows - i - 1
-    print(" "  * spaces, "#" * hashes, sep = "")
-    hashes += 2
-    #variable how many hashes to print, and change every time
+    print(" "  * (rows - i - 1), "#" * (i * 2 + 1), sep = "")
