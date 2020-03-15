586fa8856327478f1851b7617ba2e84ca8864356
namntran <namtran78@gmail.com>
2020-03-15 11:54:48 +1000

    program using Definite Loops with for loops with strings and ranges

---
+++ workshops/04/for1.py
@@ -0,0 +1,19 @@
+# Print the numbers from 0 to 4 on one line
+for i in range (5):
+    print(i, end = " ")
+print()
+
+# Print the numbers from 1 to 4 on one line
+for i in range (1,5):
+    print(i, end = " ")
+print()
+
+# Print the numbers from 0 to 9 in steps of 3 on one line
+for i in range (0,10,3):
+    print(i, end = " ")
+print()
+
+# Print the numbers from 10 down to 1 on one line
+for i in range (10,0,-1):
+    print(i, end = " ")
+print()
